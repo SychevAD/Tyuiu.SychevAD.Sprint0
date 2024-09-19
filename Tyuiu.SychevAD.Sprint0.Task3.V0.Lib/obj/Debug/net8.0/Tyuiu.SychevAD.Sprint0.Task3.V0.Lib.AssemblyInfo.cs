1f9f372dac5374f865e2a1d7894b681eb45e43b7
Sychev-Aleksandr <cool.27101978@mail.ru>
2024-09-20 01:03:45 +0500

    Task4

--- Tyuiu.SychevAD.Sprint0.Task3.V0.Lib/obj/Debug/net8.0/Tyuiu.SychevAD.Sprint0.Task3.V0.Lib.AssemblyInfo.cs
+++ Tyuiu.SychevAD.Sprint0.Task3.V0.Lib/obj/Debug/net8.0/Tyuiu.SychevAD.Sprint0.Task3.V0.Lib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.SychevAD.Sprint0.Task3.V0.Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8eee6a207d25ec2bb5349f85aa9b197bfa01ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0fdca2db0d41e40eb4a43690bc074da9c8278d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.SychevAD.Sprint0.Task3.V0.Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.SychevAD.Sprint0.Task3.V0.Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
