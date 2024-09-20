e42ea6627bc02c0bdda31c696d68659e777b01ef
Sychev-Aleksandr <cool.27101978@mail.ru>
2024-09-20 09:13:55 +0500

    Task5

--- Tyuiu.SychevAD.Sprint0.Task2.V0.Test/obj/Release/net8.0/Tyuiu.SychevAD.Sprint0.Task2.V0.Test.AssemblyInfo.cs
+++ Tyuiu.SychevAD.Sprint0.Task2.V0.Test/obj/Release/net8.0/Tyuiu.SychevAD.Sprint0.Task2.V0.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.SychevAD.Sprint0.Task2.V0.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0fdca2db0d41e40eb4a43690bc074da9c8278d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9f372dac5374f865e2a1d7894b681eb45e43b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.SychevAD.Sprint0.Task2.V0.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.SychevAD.Sprint0.Task2.V0.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Tyuiu.SychevAD.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+using Tyuiu.SychevAD.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.SychevAD.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckSubractValid()
+        {
+            Assert.AreEqual(5, DataService.Subraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+        }
+    }
